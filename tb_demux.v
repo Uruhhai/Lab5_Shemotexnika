853d644b62a1999a74911240f3b8791a46747786
Uruhhai <7540801@gmail.com>
2026-04-10 12:23:31 +0300

    Changed comments languege

--- tb_demux.v
+++ tb_demux.v
@@ -5,6 +5,7 @@ module tb_demux();
     reg [2:0] S;
     wire [7:0] Y;
 
+    // Instantiate the Unit Under Test (UUT)
     demux1to8 uut (
         .D(D),
         .S(S),
@@ -13,22 +14,23 @@ module tb_demux();
     );
 
     initial begin
+        // Console monitoring for ModelSim
         $monitor("Time=%0t | EN=%b | S=%d | D=%b | Y=%b", $time, EN, S, D, Y);
 
-        // ???? 1: ????????? ?????????????????? ????? (EN = 1)
+        // Test 1: Verify high-impedance state (EN = 1)
         EN = 1; D = 1; S = 3'd5; #10;
         
-        // ???? 2: ?????? ??? EN = 0
+        // Test 2: Normal operation (EN = 0)
         EN = 0;
         D = 1;
         S = 3'd0; #10;
         S = 3'd3; #10;
         S = 3'd7; #10;
         
-        // ???? 3: ????? ????? D
+        // Test 3: Change input data D
         D = 0; S = 3'd7; #10;
         
-        // ???? 4: ?????????? ? Z-???? ??? ??? ??????
+        // Test 4: Return to Z-state during operation
         EN = 1; #10;
 
         $stop;
